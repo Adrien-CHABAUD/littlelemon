c95113d295f554d024e5149058730a909b25466a
Adrien CHABAUD <59933787+Adrien-CHABAUD@users.noreply.github.com>
2024-01-21 22:00:08 -0500

    Initial commit

---
+++ Little Lemon/Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Adrien CHABAUD on 2024-01-21.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
