ba8cceb2b668e01b04ee70073b051b1a0d5bc66d
Adrien CHABAUD <59933787+Adrien-CHABAUD@users.noreply.github.com>
2024-01-24 20:05:20 -0500

    Regsitration Page Mechanic

--- Little Lemon/Little Lemon/Onboarding.swift
+++ Little Lemon/Little Lemon/Onboarding.swift
@@ -5,11 +5,48 @@
 //  Created by Adrien CHABAUD on 2024-01-24.
 //
 
+let kFirstName = "firstNameKey"
+let kLastName = "lastNameKey"
+let kEmail = "EmailKey"
+
 import SwiftUI
 
 struct Onboarding: View {
+    @State var firstName = ""
+    @State var lastName = ""
+    @State var email = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            TextField("First Name", text: $firstName)
+            TextField("Last Name", text: $lastName)
+            TextField("Email", text: $email)
+            
+            Button(action: {
+                if(firstName.isEmpty || lastName.isEmpty || email.isEmpty) {
+                    // TO-DO: One of the TextField is empty
+                    
+                } else {
+                    
+                    if(ValidateEmail(email: email)) {
+                        UserDefaults.standard.set(firstName, forKey: kFirstName)
+                        UserDefaults.standard.set(lastName, forKey: kLastName)
+                        UserDefaults.standard.set(email, forKey: kEmail)
+                    } else {
+                        // TO-DO: Email is not correct
+                    }
+                }
+            }, label: {
+                Text("Register")
+            })
+        }
+    }
+    
+    func ValidateEmail(email: String) -> Bool {
+        
+        let regexValue = try! NSRegularExpression(pattern: "^[A-Za-z](.*)([@])(.+)(\\.)(.+)")
+        
+        return regexValue.firstMatch(in: email, options: [], range: NSRange(location: 0, length: email.count)) != nil
     }
 }
 
