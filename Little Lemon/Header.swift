39992f8189432ca09d1fd78b9bbe307ca7c4d2cf
Adrien CHABAUD <59933787+Adrien-CHABAUD@users.noreply.github.com>
2024-01-25 23:36:40 -0500

    Adding Header file

---
+++ Little Lemon/Header.swift
@@ -0,0 +1,18 @@
+//
+//  Header.swift
+//  Little Lemon
+//
+//  Created by Adrien CHABAUD on 2024-01-25.
+//
+
+import SwiftUI
+
+struct Header: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Header()
+}
